5367d8bb9456119e149faa2815b5f35237961c04
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-05 11:56:42 +0300

    Fix;Fixing bugs in my form

--- JS/script2.js
+++ JS/script2.js
@@ -18,7 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
         //  })
 
 
-
         if (form == "") {
             alert('Please enter you BIRTHDAY DATE before submitting.');
             return false;
@@ -55,3 +54,76 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 });
+
+//document.addEventListener('DOMContentLoaded', function() {
+///document.querySelector('form').onsubmit = () => {
+//const form = document.querySelector('#form').value;
+//const date = document.querySelector('#date').value.trim();
+//const month = document.querySelector('#year').value.trim();
+//const year = document.querySelector('#date').value.trim();
+//const male = document.querySelector('#male').value;
+//const female = document.querySelector('#female').value;
+//const nd = document.querySelector('#nd').value;
+
+//const maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', ' Kofi', 'Kwame'];
+
+//const femaleName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+
+///function isDate(date) {
+///return (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) * 7
+
+//};
+
+
+///if (male == document.write(maleName[0])) {
+///alert('Your akan name is, Kwasi');
+///};
+//if (male == document.write(maleName[1])) {
+//  alert('Your akan name is,Kwadwo');
+//};
+//if (male == document.write(maleName[2])) {
+//alert('Your akan name is,Kwabena');
+//};
+//if (male == document.write(maleName[3])) {
+//    alert('Your akan name is,Kwaku');
+//};
+//if (male == document.write(maleName[4])) {
+//alert('Your akan name is,Yaw');
+//};
+//if (male == document.write(maleName[5])) {
+//alert('Your akan name is,kofi');
+//};
+///if (male == document.write(maleName[6])) {
+//alert('Your akan name is,kwame');
+//};
+
+
+
+
+
+
+//if (female == document.write(femaleName[0])) {
+///alert('Your akan name is,Akosoa');
+//};
+///if (female == document.write(femaleName[1])) {
+///alert('Your akan name is,Adwoa');
+///};
+///if (female == document.write(femaleName[2])) {
+// alert('Your akan name is,Abenaa');
+///};
+///if (female == document.write(femaleName[3])) {
+///alert('Your akan name is,Akua');
+///};
+///if (female == document.write(femaleName[4])) {
+///alert('Your akan name is,Yaa');
+//};
+///if (female == document.write(femaleName[5])) {
+///alert('Your akan name is,Afua');
+///};
+///if (female == document.write(femaleName[6])) {
+/// alert('Your akan name is,Ama');
+///};
+
+///}
+//});
