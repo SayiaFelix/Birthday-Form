d766d7680bf95d7952a1e2220a1fe542c3efaa79
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-06 21:40:57 +0300

    Fix;Fixing the bugs

--- index.js
+++ index.js
@@ -1,76 +1,66 @@
- document.addEventListener('DOMContentLoaded', function() {
-     document.querySelector('form').onsubmit = () => {
+document.querySelector('form').onsubmit = () => {
 
-         const day = document.querySelector('#day').value;
-
-         const male = document.querySelector('#male').value;
-         const female = document.querySelector('#female').value;
+    const dateInput = document.querySelector('#day').value;
 
 
+    if (dateInput <= 0 || dateInput > 31) {
+
+        alert('Your date is invalid,\n please enter the valid date');
+        return false;
+    }
+
+    if (male !== "" || female !== "") {
+        alert('Your Akan name is,Koffi');
+        return false;
+    }
+};
+
+function myFunction() {
+
+    const dateInput = document.querySelector('#day').value;
+
+    const dates = new Date(dateInput)
+    const days = dates.getDay();
+
+    console.log(days)
 
 
-         if (day <= 0 || day > 31) {
 
-             alert('Your date is invalid,\n please enter the valid date');
-             return false;
-         }
+    switch (days) {
+        case 0:
+            result.innerHTML = "Sunday";
+            break;
+        case 1:
+            result.innerHTML = "Monday";
+            break;
+        case 2:
+            result.innerHTML = "Tuesday";
+            break;
+        case 3:
+            result.innerHTML = "Wednesday";
+            break;
+        case 4:
+            result.innerHTML = "Thursday";
+            break;
+        case 5:
+            result.innerHTML = "Friday";
+            break;
+        case 6:
+            result.innerHTML = "Saturday";
+            break;
 
-         if (male !== "" || female !== "") {
-             alert('Your Akan name is,Koffi');
-             return false;
-         }
-     };
-
-
-     const day = document.querySelector('#day').value;
-     const mont = document.querySelector('#mont').value;
-     const year = document.querySelector('#year').value;
-     const result = document.getElementById('result');
-
-     function validateForm() {
-
-         const dates = new Date(day.value, mont.value, year.value);
-
-         const days = dates.getDay();
-
-         switch (days) {
-             case 0:
-                 result.innerHTML = "Sunday,kwasi";
-                 break;
-             case 1:
-                 result.innerHTML = "Monday";
-                 break;
-             case 2:
-                 result.innerHTML = "Tuesday";
-                 break;
-             case 3:
-                 result.innerHTML = "Wednesday";
-                 break;
-             case 4:
-                 result.innerHTML = "Thursday";
-                 break;
-             case 5:
-                 result.innerHTML = "Friday";
-                 break;
-             case 6:
-                 result.innerHTML = "Saturday";
-                 break;
-
-         }
-
-     }
+    }
 
 
-     newFunction();
-
-     function newFunction() {
-
-         const male = document.querySelector('#male').value;
-         const female = document.querySelector('#female').value;
-         const maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', ' Kofi', 'Kwame'];
-         const femaleName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    const result = document.querySelector('#result').value;
 
 
-     }
+    const maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
- });
+    const genderInput = document.querySelectorAll;
+    "input[name='gender]:checked"
+
+
+
+}
