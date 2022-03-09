8516823527eb652d000ef7a363438266d2910a03
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-09 09:26:38 +0300

    Fix;Fixing the bugs

--- index.js
+++ index.js
@@ -76,32 +76,44 @@ form.addEventListener('submit', (e) => {
 function validateDate() {
 
     const gender = document.getElementById('gender').value;
-    // const maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    // const femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    const akanNames = {
-        male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
-        female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
-    };
+    const maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
     let date = document.getElementById("date").value;
     if (!date) {
         return alert("Please select Your Date to continue!!!");
     }
 
-    let bornDate = new Date(date);
-    let akanNameValue = getAkanNames(akanNames, bornDate.getDay());
-
-
-
-    if (akanNameValue === "none") {
+    if (gender === "none") {
         return alert("Please select your gender to continue!!!")
 
     }
-    if (date && akanNameValue !== "none") {
+
+
+    let bornDate = new Date(date);
+    let maleNameValue = getmaleAkanNames(maleAkanNames, bornDate.getDay());
+    let femaleNameValue = getfemaleAkanNames(femaleAkanNames, bornDate.getDay());
+
+
+
+    if (date && maleNameValue !== "none") {
         let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         for (let i = 0; i < daysOfTheWeek.length; i++) {
+
             if (bornDate.getDay() === i) {
-                alert(`Born on ${daysOfTheWeek[i]}, your Akan name is ${akanNameValue}`);
+                alert(`Born on ${daysOfTheWeek[i]},\n your Akan name is ${maleNameValue}`);
+
+                document.querySelector("#gender").value = "none";
+                document.querySelector("#date").value = "";
+            }
+        }
+    }
+    if (date && femaleNameValue !== "none") {
+        let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        for (let i = 0; i < daysOfTheWeek.length; i++) {
+
+            if (bornDate.getDay() === i) {
+                alert(`Born on ${daysOfTheWeek[i]},\n your Akan name is ${femaleNameValue}`);
 
                 document.querySelector("#gender").value = "none";
                 document.querySelector("#date").value = "";
